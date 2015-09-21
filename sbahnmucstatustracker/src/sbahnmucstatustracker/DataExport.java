e906182d280bfd56e2cb652ba9a407ff99cd5502
anfi <andreas.fichtner@gmail.com>
2015-09-21 02:10:55 +0200

    DataExport working

--- sbahnmucstatustracker/src/sbahnmucstatustracker/DataExport.java
+++ sbahnmucstatustracker/src/sbahnmucstatustracker/DataExport.java
@@ -1,8 +1,12 @@
 package sbahnmucstatustracker;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -16,27 +20,61 @@ import java.util.stream.Collectors;
 public class DataExport {
 
 	private static final String SQL_SELECT_LINES = "SELECT DISTINCT LINE FROM HISTORY ORDER BY LINE";
-
-	public static void main(String[] args) throws SQLException {
+	private static final String FILENAME = "export.csv";
+	private static final String DELIMITER = ";";
+	Connection connection;
+	
+	public static void main(String[] args) throws SQLException, FileNotFoundException, UnsupportedEncodingException {
 		new DataExport().start();
 	}
 
-	public void start() throws SQLException {
+	public void start() throws SQLException, FileNotFoundException, UnsupportedEncodingException {
+		connection = DriverManager
+				.getConnection(DatabasePersister.DB_CONNECTION_STRING);
 		DatabasePersister.ensureDatabaseCreated();
 
 		List<String> lines = fetchLines();
-		Object loadedData = fetchData(lines);
-		storeToCsvFile(null, loadedData);
-	}
+		writeData(lines);
+		connection.close();
+		}
 
-	protected void storeToCsvFile(String path, Object data) {
-		// TODO
-	}
-
-	protected Object fetchData(List<String> lines) {
+	protected void writeData(List<String> lines) throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter(FILENAME, "UTF-8");
+		
 		String queryStatement = createQueryStatementToLoadData(lines);
-		// TODO
-		return null;
+		try (	Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(queryStatement);) {
+			
+			// Write header row
+			writer.print("\"Time\"");
+			for (String line : lines) {
+				writer.print(DELIMITER + "\"" + line + "\"");
+			}
+			writer.println(DELIMITER);
+			
+			// Write content
+			ResultSetMetaData rsmd = resultSet.getMetaData();
+			int columnsNumber = rsmd.getColumnCount();
+			
+			while (resultSet.next()) {
+				writer.print("\"");
+				writer.print(resultSet.getString(1));
+				writer.print("\"");
+				
+				for(int i = 2; i <= columnsNumber; i++) {
+					writer.print(DELIMITER);
+					writer.print("\"");
+					writer.print(resultSet.getString(i));
+					writer.print("\"");
+				}
+				
+				writer.println(DELIMITER);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		writer.close();
 	}
 
 	protected String createQueryStatementToLoadData(List<String> lines) {
@@ -87,17 +125,15 @@ public class DataExport {
 		List<String> lines = new ArrayList<>();
 
 		try (ResultSet result = executeQuery(SQL_SELECT_LINES)) {
-			lines.add(result.getString("LINE"));
+			while(result.next()) {
+				lines.add(result.getString("LINE"));
+			}
 		}
-
+		
 		return lines;
 	}
 
-	// TODO rni: Refactor: close the connection and statement? but not as long
-	// as the result set is in use...
 	protected ResultSet executeQuery(String sqlQuery) throws SQLException {
-		Connection connection = DriverManager
-				.getConnection(DatabasePersister.DB_CONNECTION_STRING);
 		Statement statement = connection.createStatement();
 		return statement.executeQuery(sqlQuery);
 	}
